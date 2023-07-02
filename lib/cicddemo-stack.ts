ca3ef22894b1a6559995305aace53334a314990b
pranita <tonz123@gmail.com>
2023-07-02 12:16:11 +0530

    Your commit message

---
+++ lib/cicddemo-stack.ts
@@ -0,0 +1,28 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+
+export class CicddemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    //AWS CI-CD Pipleine
+
+    const medemopipleline = new CodePipeline(this, 'logicalidpipe', {
+      synth: new ShellStep('Synth', {
+        // Use a connection created using the AWS console to authenticate to GitHub
+        input: CodePipelineSource.gitHub('tanmoygope/cicddemo', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
+
+    })
+
+  }
+}
---
+++ test/cicddemo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cicddemo from '../lib/cicddemo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cicddemo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cicddemo.CicddemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
