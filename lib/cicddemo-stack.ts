660ebbbde5e09f525e38a5fd80640ee95d8ffe5b
pranita <tonz123@gmail.com>
2023-07-02 16:24:01 +0530

    Your descriptive commit message here

--- lib/cicddemo-stack.ts
+++ lib/cicddemo-stack.ts
@@ -1,27 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class CicddemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-    //AWS CI-CD Pipleine
-
-    const medemopipleline = new CodePipeline(this, 'logicalidpipe', {
-      synth: new ShellStep('Synth', {
-        // Use a connection created using the AWS console to authenticate to GitHub
-        input: CodePipelineSource.gitHub('tanmoygope/cicdpipe', 'main'),
-        commands: [
-          'npm ci',
-          'npm run build',
-          'npx cdk synth',
-        ],
-      }),
+    const synth = new ShellStep('Synth', {
+      input: CodePipelineSource.gitHub('tanmoygope/cicdpipe', 'main'),
+      commands: [
+        'npm ci',
+        'npm run build',
+        'npx cdk synth',
+      ],
     });
 
+    new CodePipeline(this, 'logicalidpipe', { synth });
   }
 }
